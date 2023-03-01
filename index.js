956a10b2a6329feafe1fe3072451a59e367e9b2c
Alex Townson <59401739+alextownson@users.noreply.github.com>
2023-03-01 10:33:49 -0500

    Improved index responsive design.

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ import { backgroundStars } from './modules/stars.js';
 
 backgroundStars()
 
-window.addEventListener('resize', backgroundStars)
+window.addEventListener('resize', () => {
+    backgroundStars()
+    document.querySelector('#menu').style.height = window.innerHeight + 'px';
+})
+
+window.addEventListener('load', () => {
+    document.querySelector('#menu').style.height = window.innerHeight + 'px';
+})
